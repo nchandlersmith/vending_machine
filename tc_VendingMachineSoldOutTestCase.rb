2b9e1d564b75e6e6eded57f41d7d44c868967d02
Nate Chandler-Smith <nate.chandlersmith@gmail.com>
2018-05-20 17:11:32 -0500

    added test cases to cover attempts to purchase below and above price and also added candy sold out feature

--- tc_VendingMachineSoldOutTestCase.rb
+++ tc_VendingMachineSoldOutTestCase.rb
@@ -19,12 +19,22 @@ class SoldOutTest < VendingMachineTest
     verifyPurchaseAttemptWhenSoldOut()
   end
 
+  def checkCoinReturnForFiveQuarters()
+    @vending_machine.requestCoinReturn()
+    returnInCoins = @vending_machine.checkCoinReturn()
+    for i in 0..4
+      assert_instance_of(Quarter, returnInCoins[i])
+    end
+    assert_equal(5, returnInCoins.count())
+    assert_equal("INSERT COIN", @vending_machine.checkDisplay())
+  end
+
   def attemptCandyPurchaseWhenSoldOut()
     @vending_machine.candyButtonPressed()
     verifyPurchaseAttemptWhenSoldOut()
   end
 
-  def test_VendingMachineSoldOutPurchaseColaAtPrice
+  def test_SoldOutAttemptPurchaseColaAtPrice
     @vending_machine.adjustColaStock(-1)
     for i in 1..4
       insertCoinAndVerifyDisplay(@quarter)
@@ -32,20 +42,65 @@ class SoldOutTest < VendingMachineTest
     attemptColaPurchaseWhenSoldOut()
   end
 
-  def test_VendingMachineSoldOutPurchaseChipsAtPrice
+  def test_SoldOutAttemptPurchaseChipsAtPrice
     @vending_machine.adjustChipsStock(-1)
     insertCoinAndVerifyDisplay(@quarter)
     insertCoinAndVerifyDisplay(@quarter)
     attemptChipsPurchaseWhenSoldOut()
   end
 
-  def test_VendingMachineSoldOutPurchaseCandyAtPrice
+  def test_SoldOutAttemptPurchaseCandyAtPrice
     @vending_machine.adjustCandyStock(-1)
     insertCoinAndVerifyDisplay(@quarter)
     insertCoinAndVerifyDisplay(@quarter)
     insertCoinAndVerifyDisplay(@dime)
     insertCoinAndVerifyDisplay(@nickel)
     attemptCandyPurchaseWhenSoldOut()
+  end
+
+  def test_SoldOutAttemptPurchaseCandyBelowPrice
+    @vending_machine.adjustCandyStock(-1)
+    insertCoinAndVerifyDisplay(@quarter)
+    attemptCandyPurchaseWhenSoldOut
+  end
+
+  def test_SoldOutAttemptPurchaseChipsBelowPrice
+    @vending_machine.adjustChipsStock(-1)
+    insertCoinAndVerifyDisplay(@nickel)
+    attemptChipsPurchaseWhenSoldOut
+  end
+
+  def test_SoldOutAttemptPurchaseColaBelowPrice
+    @vending_machine.adjustColaStock(-1)
+    insertCoinAndVerifyDisplay(@dime)
+    attemptColaPurchaseWhenSoldOut()
+  end
+
+  def test_SoldOutAttemptPurchaseCandyAbovePrice
+    @vending_machine.adjustCandyStock(-1)
+    for i in 1..5
+      insertCoinAndVerifyDisplay(@quarter)
+    end
+    attemptCandyPurchaseWhenSoldOut()
+    checkCoinReturnForFiveQuarters()
+  end
+
+  def test_SoldOutAttemptPurchaseChipsAbovePrice
+    @vending_machine.adjustChipsStock(-1)
+    for i in 1..5
+      insertCoinAndVerifyDisplay(@quarter)
+    end
+    attemptChipsPurchaseWhenSoldOut()
+    checkCoinReturnForFiveQuarters()
+  end
+
+  def test_SoldOutAttemptPurchaseColaAbovePrice
+    @vending_machine.adjustColaStock(-1)
+    for i in 1..5
+      insertCoinAndVerifyDisplay(@quarter)
+    end
+    attemptColaPurchaseWhenSoldOut()
+    checkCoinReturnForFiveQuarters()
   end
 
 end
