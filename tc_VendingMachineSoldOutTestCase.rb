b0657b1e5855f4559c4c9fba541c54f06301f0d7
Nate Chandler-Smith <nate.chandlersmith@gmail.com>
2018-05-20 16:45:42 -0500

    fixed bug: now checking for amount inserted not price of sold out item; refactored test

--- tc_VendingMachineSoldOutTestCase.rb
+++ tc_VendingMachineSoldOutTestCase.rb
@@ -3,27 +3,35 @@ require_relative "VendingMachineTestClass"
 
 class SoldOutTest < VendingMachineTest
 
-  def test_VendingMachineSoldOutCola
+  def verifyPurchaseAttemptWhenSoldOut()
+    assert_equal("SOLD OUT", @vending_machine.checkDisplay())
+    assert_equal("\$%0.2f" % [@amountMoneyAccepted], @vending_machine.checkDisplay())
+    assert_equal("\$%0.2f" % [@amountMoneyAccepted], @vending_machine.checkDisplay())
+  end
+
+  def attemptColaPurchaseWhenSoldOut()
+    @vending_machine.colaButtonPressed()
+    verifyPurchaseAttemptWhenSoldOut()
+  end
+
+  def attemptChipsPurchaseWhenSoldOut()
+    @vending_machine.chipsButtonPressed()
+    verifyPurchaseAttemptWhenSoldOut()
+  end
+
+  def test_VendingMachineSoldOutPurchaseColaAtPrice
     @vending_machine.adjustColaStock(-1)
     for i in 1..4
       insertCoinAndVerifyDisplay(@quarter)
     end
-    @vending_machine.colaButtonPressed()
-    assert_equal("SOLD OUT", @vending_machine.checkDisplay())
-    assert_equal("$1.00", @vending_machine.checkDisplay())
-    assert_equal("$1.00", @vending_machine.checkDisplay())
+    attemptColaPurchaseWhenSoldOut()
   end
 
-  def test_VendingMachineSoldOutChips
+  def test_VendingMachineSoldOutPurchaseChipsAtPrice
     @vending_machine.adjustChipsStock(-1)
     insertCoinAndVerifyDisplay(@quarter)
     insertCoinAndVerifyDisplay(@quarter)
-    insertCoinAndVerifyDisplay(@dime)
-    insertCoinAndVerifyDisplay(@nickel)
-    @vending_machine.chipsButtonPressed()
-    assert_equal("SOLD OUT", @vending_machine.checkDisplay())
-    assert_equal("$0.65", @vending_machine.checkDisplay())
-    assert_equal("$0.65", @vending_machine.checkDisplay())
+    attemptChipsPurchaseWhenSoldOut()
   end
 
 end
